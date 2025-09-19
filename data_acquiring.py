50022b6acd2fe77689c27e314c4544d63b0f3256
Luis-Menezes <luislipe4@gmail.com>
2025-09-19 10:23:15 -0300

    Initial commit with dataset acquiring script

---
+++ data_acquiring.py
@@ -0,0 +1,13 @@
+import kagglehub
+import pandas as pd
+
+# Download latest version
+def download_and_load_dataset():
+    path = kagglehub.dataset_download("zadafiyabhrami/global-crocodile-species-dataset") + "/crocodile_dataset.csv"
+    df = pd.read_csv(path)
+    return df
+
+if __name__ == "__main__":
+    df = download_and_load_dataset()
+    print("Dataset preview:\n", df.head())
+    # print("Files in dataset:", kagglehub.list_files(path))
